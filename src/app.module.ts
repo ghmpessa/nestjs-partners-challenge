8de8119ab3a6c6cb1600755b7dc89800393c1a0f
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-19 22:47:59 -0300

    refactor: remove unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [EventsModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
