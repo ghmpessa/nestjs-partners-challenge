dc1ef2cf716336986902c35d10ca6df8d395c4cf
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-19 22:15:58 -0300

    refactor: add validation decorators to CreateEventDto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,6 +1,22 @@
+import { IsDateString, IsNotEmpty, IsNumber, IsString, MaxLength, Min } from "class-validator";
+
 export class CreateEventDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
   description: string;
+
+  @IsNotEmpty()
+  @IsDateString()
   date: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   price: number;
 }
