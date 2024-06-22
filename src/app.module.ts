8ec57307dba85d19cd948790ec92e140e35b227f
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-22 18:18:07 -0300

    feat: add ConfigModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SpotsModule } from './spots/spots.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EventsModule, PrismaModule, SpotsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    EventsModule,
+    SpotsModule,
+  ],
 })
 export class AppModule { }
